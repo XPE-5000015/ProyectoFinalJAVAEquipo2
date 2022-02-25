b2714c667b763b90b8314f8313c9c69e2613eef6
XPE-5000015 <juan.palomo@xpertal.com>
2022-02-25 15:13:43 -0600

    Se agrega la anotación Table para indicar el nombre de la tabala en MySQL.

--- src/main/java/com/example/demo/entity/Hotel_booking.java
+++ src/main/java/com/example/demo/entity/Hotel_booking.java
@@ -4,16 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "hotel_bookings")
 public class Hotel_booking {
 
     @Id
