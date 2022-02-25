ef7a3e8e3ece4a417a2b369041711c4e5b11dfd2
mauriciotapia21 <mauricio.tapia@xpertal.com>
2022-02-25 14:59:28 -0600

    Se capto el error del id que estaba mal escrito

--- src/main/java/com/example/demo/entity/Reservation_person.java
+++ src/main/java/com/example/demo/entity/Reservation_person.java
@@ -14,6 +14,6 @@ import javax.persistence.*;
 public class Reservation_person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer reservation_person;
+    private Integer reservation_id;
     private String dni;
 }
