80e0219412798c2524857b1d14611a244c80aeab
XPE-5000015 <juan.palomo@xpertal.com>
2022-02-28 10:07:25 -0600

    Se coloca el IdClass, y se implenta Serializable para indicar el id compuesto.

--- src/main/java/com/example/demo/entity/Reservation_person.java
+++ src/main/java/com/example/demo/entity/Reservation_person.java
@@ -5,13 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "reservation_person")
-public class Reservation_person {
+@IdClass(Reservation_person.class)
+public class Reservation_person implements Serializable {
     @Id
     private Integer reservation_id;
     @Id
