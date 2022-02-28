02a5795aa7394ab3a227213d172be8164c5bfe51
mauriciotapia21 <mauricio.tapia@xpertal.com>
2022-02-28 14:27:50 -0600

    se Ajusto el  BookingPayloadDTO al payload

--- src/main/java/com/example/demo/dto/BookingPayloadDTO.java
+++ src/main/java/com/example/demo/dto/BookingPayloadDTO.java
@@ -1,10 +1,8 @@
 package com.example.demo.dto;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
@@ -14,14 +12,14 @@ import java.util.List;
 @NoArgsConstructor
 public class BookingPayloadDTO {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    LocalDate dateFrom;
+    private LocalDate dateFrom;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    LocalDate dateTo;
-    String destination;
-    String hotelCode;
-    Integer peopleAmount;
-    String roomType;
+    private LocalDate dateTo;
+    private String destination;
+    private String hotelCode;
+    private Integer peopleAmount;
+    private String roomType;
     @Valid
-    List<PersonDTO> people;
-    PaymentMethodDTO paymentMethod;
+    private List<PersonDTO> people;
+    private PaymentMethodDTO paymentMethod;
 }
