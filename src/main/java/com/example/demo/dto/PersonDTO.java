df9b888bd3baa0b63753cbcc681a1a43e46d986f
Jandres98 <jorgea.martinez@xpertal.com>
2022-02-28 15:44:13 -0600

    actualizacion de develop con el feature/E06

--- src/main/java/com/example/demo/dto/PersonDTO.java
+++ src/main/java/com/example/demo/dto/PersonDTO.java
@@ -11,11 +11,11 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PersonDTO {
-    String dni;
-    String name;
-    String lastname;
+    private String dni;
+    private String name;
+    private String lastname;
     @Pattern(regexp = "^(0[1-9]|[12][0-9]|[3][01])/(0[1-9]|1[012])/\\d{4}$",message = "Formato de fecha debe ser dd/mm/aaaa.")
-    String birthDate;
+    private String birthDate;
     @Email(regexp = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", message = "Por favor ingrese un e-mail válido.")
-    String mail;
+    private String mail;
 }
