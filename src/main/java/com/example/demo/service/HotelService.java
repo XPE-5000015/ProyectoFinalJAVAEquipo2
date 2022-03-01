09f88dfe1ceed17c62d770061d3e326a7f631176
mauriciotapia21 <mauricio.tapia@xpertal.com>
2022-03-01 17:35:37 -0600

    Se quito el import de entity en el Service de Hotel

--- src/main/java/com/example/demo/service/HotelService.java
+++ src/main/java/com/example/demo/service/HotelService.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import com.example.demo.dto.HotelDTO;
 import com.example.demo.dto.PayloadHotelDTO;
 import com.example.demo.dto.ResponseHotelDTO;
-import com.example.demo.entity.Hotel;
 
 import java.time.LocalDate;
 import java.util.List;
